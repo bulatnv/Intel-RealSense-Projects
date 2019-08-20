a37ab64cf6778c0a659cc13a3280009c3aad1e4d
bulatnv88 <bulatnv@gmail.com>
2019-08-20 14:49:02 +0100

    chached folder names, added gitignore

---
+++ Depth-and-Color-to-png/build/CMakeFiles/PostProcessingDepthandColorCapturing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PostProcessingDepthandColorCapturing.dir/main.cpp.o"
+  "PostProcessingDepthandColorCapturing.pdb"
+  "PostProcessingDepthandColorCapturing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PostProcessingDepthandColorCapturing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
